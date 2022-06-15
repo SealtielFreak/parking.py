235e92f814454953e6bda59bd63115d47e6e2fdb
SealtielFreak <43727474+SealtielFreak@users.noreply.github.com>
2022-06-15 00:26:08 -0500

    urls adapting for api rest

--- api/urls.py
+++ api/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import api.views
 
 urlpatterns = [
-    path('api/', include("api.urls")),
-    path('admin/', admin.site.urls),
+    path('clients/', api.views.RequestClients.as_view(), name="clients_list"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpRequest, JsonResponse
@@ -7,11 +9,9 @@ import api.models
 
 
 class RequestClients(View):
-
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
-
 
     def get(self, request: HttpRequest):
         clients = list(api.models.Client.objects.values())
@@ -19,4 +19,6 @@ class RequestClients(View):
         return JsonResponse({"message": "Success", "clients": f"{clients}"} if not len(clients) == 0 else { "message": "Failure" } )
 
     def post(self, request: HttpRequest):
-        return JsonResponse({ "message": "Success", "body": f"{str(request.body)}"})
+        data = request.body
+
+        return JsonResponse({ "message": "Success", "body": data})
